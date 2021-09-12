ec55c2ef41d4d247f425326b6736f2738c8e633e
Salvo Dragotta <salvo.dragotta@mottmac.com>
2021-09-12 16:37:03 +1000

    Fix Create Post front end route, add style to form

--- public/js/createPost.js
+++ public/js/createPost.js
@@ -2,12 +2,12 @@ const createPostFormHandler = async (event) => {
   event.preventDefault();
 
   const title = document.querySelector('input[name="create-post-title"]').value;
-  const post_content = document.querySelector('input[name="create-post-content"]').value;
+  const content = document.querySelector('input[name="create-post-content"]').value;
 
-  if (title  && post_content) {
+  if (title  && content) {
     const response = await fetch('/api/posts', {
       method: 'POST',
-      body: JSON.stringify({ title , post_content }),
+      body: JSON.stringify({ title , content }),
       headers: { 'Content-Type': 'application/json' },
     });
 
