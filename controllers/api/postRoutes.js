41e5c6271bfa204b2201d9c2ab3fdbc3f5a4c84f
Salvo Dragotta <salvo.dragotta@mottmac.com>
2021-09-12 13:32:28 +1000

    Minor Changes

---
+++ controllers/api/postRoutes.js
@@ -0,0 +1,20 @@
+
+const router = require('express').Router();
+const { Post, User, Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+router.post('/', withAuth, async (req, res) => {
+    try {
+        const createPost =  Post.create(
+            {
+                ...req.body,
+                user_id: req.session.user_id,
+            }
+        );
+
+        res.status(200).json(createPost);
+    } catch (err) {
+            console.log(err.message);
+            res.status(500).json(err);
+        }
+});
---
+++ public/js/createPost.js
@@ -0,0 +1,24 @@
+const createPostFormHandler = async (event) => {
+  event.preventDefault();
+
+  const title = document.querySelector('input[name="create-post-title"]').value;
+  const post_content = document.querySelector('input[name="create-post-content"]').value;
+
+  if (title  && post_content) {
+    const response = await fetch('/api/posts', {
+      method: 'POST',
+      body: JSON.stringify({ title , post_content }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
+
+document
+  .querySelector('.create-post')
+  .addEventListener('submit', createPostFormHandler);
