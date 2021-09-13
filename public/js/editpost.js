9006117c0750d149647a7dd75036cb3e667a1ed3
Salvo Dragotta <salvo.dragotta@mottmac.com>
2021-09-13 11:29:22 +1000

    Fix bug

--- public/js/editpost.js
+++ public/js/editpost.js
@@ -2,7 +2,7 @@ async function editFormHandler(event) {
     event.preventDefault();
   
     const title = document.querySelector('input[name="post-title"]').value;
-    const content = document.querySelector('input[name="post-content"]').value;
+    const content = document.querySelector('textarea[name="post-content"]').value;
     const id = window.location.toString().split('/')[
         window.location.toString().split('/').length - 1
       ];
